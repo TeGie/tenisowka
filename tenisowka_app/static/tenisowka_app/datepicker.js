11694fed424c5456849a2bf9f846add6b644c884
TeGie <tegie@tegie.com>
2017-09-23 18:10:45 +0200

    timepicker added

--- tenisowka_app/static/tenisowka_app/datepicker.js
+++ tenisowka_app/static/tenisowka_app/datepicker.js
@@ -1,5 +1,14 @@
 $(function() {
-    $( "#id_data_badania, #id_data_urodzenia, #id_data, #id_start, #id_koniec" ).datepicker({
+    $( "#id_data_badania, #id_data_urodzenia, #id_data" ).datepicker({
+        changeYear: true,
+        maxDate: '30',
+        dayNamesMin: [ "Nd", "Pn", "Wt", "Śr", "Czw", "Pt", "So" ],
+        monthNames: [ "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj",
+        "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Pażdziernik",
+        "Listopad", "Grudzień" ],
+    });
+
+    $( "#id_start, #id_koniec" ).datetimepicker({
         changeYear: true,
         maxDate: '30',
         dayNamesMin: [ "Nd", "Pn", "Wt", "Śr", "Czw", "Pt", "So" ],
